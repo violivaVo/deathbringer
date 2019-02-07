264c48f06a2ba5759b905ab327466836eb418766
Mauro Bussini <mauro.bussini@hotmail.it>
2019-02-07 11:35:03 +0100

    Pulita classe ProdottoVenduto

--- DeathBringer.Terminal/Entities/ProdottoVenduto.cs
+++ DeathBringer.Terminal/Entities/ProdottoVenduto.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using DeathBringer.Terminal.BaseClasses;
+﻿using DeathBringer.Terminal.BaseClasses;
 
 namespace DeathBringer.Terminal.Entities
 {
-    class ProdottoVenduto: EntityBase
+    public class ProdottoVenduto: EntityBase
     {
         public int Quantita { get; set; }
         public Prezzo PrezzoProdotto { get; set; }
-        public Carrello CarrelloAssociato { get; set; }
+        public Carrello CarrelloAssociato { get; set; }        
     }
 }

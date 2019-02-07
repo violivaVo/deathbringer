c233b7dd4f0e14c1ed0bbb41655db05407eef097
Maurizio <Guest104@TRAINING104>
2019-02-07 15:39:44 +0100

    corretto inserimento prodotto

--- DeathBringer.Core/ServiceLayers/ProdottiServiceLayer.cs
+++ DeathBringer.Core/ServiceLayers/ProdottiServiceLayer.cs
@@ -8,7 +8,7 @@ using System.Text;
 
 namespace DeathBringer.Core.ServiceLayers
 {
-    class ProdottiServiceLayer
+    class ProdottiServiceLayer 
     {
         public IList<Prodotto> FetchProdotti()
         {
@@ -21,35 +21,21 @@ namespace DeathBringer.Core.ServiceLayers
             return ApplicationStorage.Prodotti.SingleOrDefault(n => n.Id == id);
         }
 
-        public IList<ValidationResult> InsertProdotto(string name, string description)
+        public IList<ValidationResult> InsertProdotto(string name, string descr)
         {
-            //Preparo la lista vuota che è simbolo di successo dell'operazione
             IList<ValidationResult> validations = new List<ValidationResult>();
-
-            //Se il nome (che è OBBLIGATORIO) è vuoto o nullo, esco
             if (string.IsNullOrWhiteSpace(name))
             {
-                //Aggiungo il messaggio con la spiegazione ed esco
                 validations.Add(new ValidationResult($"Il nome è obbligatorio"));
                 return validations;
             }
-
-            //Creazione dell'oggetto (classe)
             var nuovoProdotto = new Prodotto
             {
                 Id = GeneratoreId.GeneraNuovoIdentificatore<Prodotto>(ApplicationStorage.Prodotti),
                 Nome = name,
-                Descrizione = description,
-                DataCreazioneRecord = DateTime.Now,
-                DataUltimaModifica = DateTime.Now,
-                UtenteCreazioneRecord = "anonymous",
-                UtenteUltimaModificaRecord = "anonymous"
+                Descrizione  = descr
             };
-
-            //Aggiunta nella lista generale
             ApplicationStorage.Prodotti.Add(nuovoProdotto);
-
-            //Mando in uscita le validazioni (VUOTE) per segnalare che è tutto ok
             return validations;
         }
 

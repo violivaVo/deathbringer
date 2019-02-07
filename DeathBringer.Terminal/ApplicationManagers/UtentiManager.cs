855e490328a5da2b46023dfaf583f87ffa90a18b
Guest110 <violiva.vo@gmail.com>
2019-02-07 10:02:52 +0100

    aggiunto switch

--- DeathBringer.Terminal/ApplicationManagers/UtentiManager.cs
+++ DeathBringer.Terminal/ApplicationManagers/UtentiManager.cs
@@ -6,15 +6,79 @@ namespace DeathBringer.Terminal.ApplicationManagers
 {
    public class UtentiManager
     {
+        static string selezioneU = null;
         public static void VisualizzaMenu()
         {
-            Console.WriteLine("*****Gestione utenti******");
-            Console.WriteLine("1 - Inserisci Utente");
-            Console.WriteLine("2 - Modifica Utente");
-            Console.WriteLine("3 - Rimuovi Utente");
-            Console.WriteLine("4 - Elenco Utenti");
-            Console.WriteLine("5 - Login");
-            Console.ReadLine();
+            while (selezioneU != "exit")
+            {
+
+                //visualizzo a terminale l'elenco delle funzione del mio programma
+                Console.WriteLine("******");
+                Console.WriteLine("Utenti Manager");
+                Console.WriteLine("*********************************");
+                Console.WriteLine();
+                Console.WriteLine("1 - Crea Utente");
+                Console.WriteLine("2 - Modifica Utente");
+                Console.WriteLine("3 - Cancella Utente");
+                Console.WriteLine("4 - Elenco Utenti");
+                Console.WriteLine("5 - Login");
+                Console.WriteLine("exit => uscita");
+                Console.WriteLine();
+                Console.Write(" selezione: ");
+
+                //leggo la selezione
+                selezioneU = Console.ReadLine();  //nota (NON PIù, POI L'HO DEF. PRIMA) il tipo var: sta avvenendo un'assegnazione diretta, quindi non è necessario indicare prima il suo tipo ( è un pigliatutto)
+                                                 //utile specialmente quando ci sono tipi complessi lunghi, o anche molti (troppi) tipi
+
+                //seleziona la funzione giusta
+                switch (selezioneU)
+                {
+                    case "1":
+                        CreaUtente();
+                        break;
+                    case "2":
+                        ModificaUtente();
+                        break;
+                    case "3":
+                        EliminaUtente();
+                        break;
+                    case "4":
+                        ElencoUtenti();
+                        break;
+                    case "5":
+                        Login();
+                        break;
+                    default:  //equivale all'else, ossia se non  si è trattato di nessuno dei casi sovraindicati, di suo fai fare questo default
+                        Console.WriteLine("Selezione non valida!");
+                        break;
+                }
+
+            }
+        }
+
+        private static void Login()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void CreaUtente()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ModificaUtente()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void EliminaUtente()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ElencoUtenti()
+        {
+            throw new NotImplementedException();
         }
     }
 }

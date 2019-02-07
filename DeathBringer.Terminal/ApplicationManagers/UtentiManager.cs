f523cc1e69777bfd8d91d5ac0502522b6f84ba88
Bilel <benabdallahbilel@gmail.com>
2019-02-07 11:46:03 +0100

    merge

--- DeathBringer.Terminal/ApplicationManagers/UtentiManager.cs
+++ DeathBringer.Terminal/ApplicationManagers/UtentiManager.cs
@@ -2,6 +2,7 @@
 using DeathBringer.Terminal.Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DeathBringer.Terminal.ApplicationManagers

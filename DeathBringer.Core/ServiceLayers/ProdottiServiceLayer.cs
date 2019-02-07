1f6ca772dca001516c13b30c7065a09cdabe2aa2
giuliohub <guest102@TRAINING105>
2019-02-07 14:43:12 +0100

    aggiunti tutti i metodi di prodottiservivelayer

--- DeathBringer.Core/ServiceLayers/ProdottiServiceLayer.cs
+++ DeathBringer.Core/ServiceLayers/ProdottiServiceLayer.cs
@@ -1,6 +1,7 @@
 ﻿using DeathBringer.Terminal.BaseClasses;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace DeathBringer.Core.ServiceLayers
@@ -9,12 +10,31 @@ namespace DeathBringer.Core.ServiceLayers
     {
         public IList<Prodotto> FetchProdotti()
         {
-         
+
         }
 
         public Prodotto GetProdotto(int id)
         {
-            
+
+        }
+
+        public IList<ValidationResult> InsertProdotto(string name, string description)
+        {
+
+        }
+
+
+
+
+
+        public IList<ValidationResult> UpdateProdotto(int id, string name, string description)
+        {
+
+        }
+
+        public IList<ValidationResult> DeleteProdotto(int id)
+        {
+
         }
     }
 }

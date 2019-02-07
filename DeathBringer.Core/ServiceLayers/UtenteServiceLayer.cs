8d756be3961c4a374973623577a0cba2cc76f958
Bilel <benabdallahbilel@gmail.com>
2019-02-07 14:38:05 +0100

    Bilel : Fetchutente

--- DeathBringer.Core/ServiceLayers/UtenteServiceLayer.cs
+++ DeathBringer.Core/ServiceLayers/UtenteServiceLayer.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using DeathBringer.Terminal.Data;
+using DeathBringer.Terminal.Entities;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DeathBringer.Core.ServiceLayers
 {
     class UtenteServiceLayer
     {
+        public IList<Utente> FetchUtenti()
+        {
+            return ApplicationStorage.Utenti
+                .OrderBy(e => e.Nome)
+                .ThenBy(e => e.Indirizzo)
+                .ToList();
+        }
+       
     }
 }

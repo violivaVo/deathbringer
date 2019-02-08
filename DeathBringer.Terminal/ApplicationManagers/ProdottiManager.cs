c17e391afea3fbeee97c58030147916009d92811
Maurizio <Guest104@TRAINING104>
2019-02-08 13:05:40 +0100

    Eliminato file inserimentoprodotto.cs

--- DeathBringer.Terminal/ApplicationManagers/ProdottiManager.cs
+++ DeathBringer.Terminal/ApplicationManagers/ProdottiManager.cs
@@ -1,5 +1,6 @@
 ﻿using DeathBringer.Core.ServiceLayers;
 using DeathBringer.Terminal.BaseClasses;
+using DeathBringer.Terminal.Data;
 using DeathBringer.Terminal.Entities;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,8 @@ namespace DeathBringer.Terminal.ApplicationManagers
 {
     public class ProdottiManager
     {
+        private static Categoria categoriaClasse;
+
         internal static void Indice()
         {
             string selection = null;
@@ -56,7 +59,51 @@ namespace DeathBringer.Terminal.ApplicationManagers
         {
             var Layer = new ProdottiServiceLayer();
             IList<Prodotto> prodotti = Layer.FetchProdotti();
+            Console.WriteLine("Creazione nuovo prodotto");
+            Console.WriteLine(" => nome : ");
+            var nome = Console.ReadLine();
+            Console.WriteLine(" => categoria : ");
+            var categoryString = Console.ReadLine();
+            int m = ApplicationStorage.Prodotti.Count;
+            for (int i = 0; i < m; i++)
+            {
+                string category = ApplicationStorage.Categorie[i].Nome;
+                Categoria categoriaClasse = new Categoria();
+                if (category == categoryString)
+                    categoriaClasse = ApplicationStorage.Categorie[i];
+            };
+
+            Console.WriteLine("Data Produzione: ");
+            var dataProduzioneString = Console.ReadLine();
+            DateTime dataProduzione = Convert.ToDateTime(dataProduzioneString);
+            Console.WriteLine("Inserisci descrizione: ");
+            string descrizione = Console.ReadLine();
+            Console.WriteLine("Inserisci brand: ");
+            string brand = Console.ReadLine();
+
+            Prodotto nuovoProdotto = new Prodotto()
+            {
+                Id = GeneraNuovoIdentificatore(),
+                Nome = nome,
+                CategoriaAppartenenza = categoriaClasse,
+                DataProduzione = dataProduzione,
+                Descrizione = descrizione,
+                Brand = brand
+            };
+
+            ApplicationStorage.Prodotti.Add(nuovoProdotto);
+
+            Console.WriteLine($"Creato prodotto {nuovoProdotto.Nome}!"); //oppure concateni, ya know, ma conviene questo modo moderno
+            Console.ReadLine();
+
+
         }
+
+        private static int GeneraNuovoIdentificatore()
+        {
+            throw new NotImplementedException();
+        }
+
         internal static void ElencoProdotti()
         {
 

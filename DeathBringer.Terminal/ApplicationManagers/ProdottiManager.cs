e7b37aa77f9d23206234afe33941f364326fa5af
Maurizio <Guest104@TRAINING104>
2019-02-08 14:31:52 +0100

    Ok prodottimanager

--- DeathBringer.Terminal/ApplicationManagers/ProdottiManager.cs
+++ DeathBringer.Terminal/ApplicationManagers/ProdottiManager.cs
@@ -4,6 +4,7 @@ using DeathBringer.Terminal.Data;
 using DeathBringer.Terminal.Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DeathBringer.Terminal.ApplicationManagers
@@ -83,7 +84,7 @@ namespace DeathBringer.Terminal.ApplicationManagers
 
             Prodotto nuovoProdotto = new Prodotto()
             {
-                Id = GeneraNuovoIdentificatore(),
+                Id = GeneratoreId.GeneraNuovoIdentificatore<Prodotto>(ApplicationStorage.Prodotti),
                 Nome = nome,
                 CategoriaAppartenenza = categoriaClasse,
                 DataProduzione = dataProduzione,
@@ -99,21 +100,97 @@ namespace DeathBringer.Terminal.ApplicationManagers
 
         }
 
-        private static int GeneraNuovoIdentificatore()
-        {
-            throw new NotImplementedException();
-        }
-
         internal static void ElencoProdotti()
         {
-
+            //recupera quelli in memoria
+            var prodottiRecuperatiDallaMemoria = ApplicationStorage.Prodotti;
+            // cicla sulle categorie in memoria
+            for (int i = 0; i < prodottiRecuperatiDallaMemoria.Count; i++)
+            {
+                // Visualizza dati Utente
+                Console.WriteLine(
+                    $"nome: {prodottiRecuperatiDallaMemoria[i].Nome}, " +    //accedo all'i-esimo elemento della lista, e prendo il suo Nome
+                    $"descrizione: {prodottiRecuperatiDallaMemoria[i].Descrizione}, "
+                    );
+            }
         }
+
         internal static void EliminaProdotto()
         {
+            Console.WriteLine("[ ---------------------------- ]");
+            Console.WriteLine("[ Elimina prodotto esistente ]");
 
+            Console.Write("Inserisci id prodotto da eliminare: ");
+            Prodotto prodottoDaProcessare = ReadProdottoFromConsole();
+
+            //Rimozione della categoria dalla lista
+            if (prodottoDaProcessare == null)
+            {
+                return;
+            }
+            else
+            {
+                ApplicationStorage.Prodotti.Remove(prodottoDaProcessare);
+                Console.WriteLine("Il prodotto è stato cancellato!!!");
+            }
         }
+
         internal static void ModificaProdotto()
         {
+            Console.WriteLine("[ ---------------------------- ]");
+            Console.WriteLine("[ Modifica prodotto esistente ]");
+
+            Console.Write("Inserisci id prodotto da modificare: ");
+            Prodotto prodottoDaProcessare = ReadProdottoFromConsole();
+
+            if (prodottoDaProcessare == null)
+            {
+                return;
+            }
+            else
+            {
+                //Richiedo il nuovo nome
+                Console.Write(" => nuovo nome: ");
+                var nuovoNome = Console.ReadLine();
+                Console.Write(" => nuovo descr: ");
+                var nuovaDescrizione = Console.ReadLine();
+                
+
+                //Assegnamento ad oggetto esistente
+                prodottoDaProcessare.Nome = nuovoNome;
+                prodottoDaProcessare.Descrizione = nuovaDescrizione;
+
+                Console.WriteLine("La modifica è stata completata!");
+            }
+        }
+
+        private static Prodotto ReadProdottoFromConsole()
+        {
+            var id = Console.ReadLine();
+            int idIntero;
+
+            if (!int.TryParse(id, out idIntero))
+            {
+                Console.WriteLine("Il valore inserito non è valido!");
+                return null;
+            }
+
+            else
+            {  
+                Prodotto prodottoEsistente = ApplicationStorage.Prodotti
+                    .SingleOrDefault(prodottoCorrente => prodottoCorrente.Id == idIntero);
+
+                if (prodottoEsistente == null)
+                {
+                    Console.WriteLine("Il prodotto selezionato non esiste!");
+                    return null;
+                }
+                else
+                {
+                    Console.WriteLine("Il prodotto selezionato esiste!");
+                    return prodottoEsistente;
+                }
+            }
 
         }
     }

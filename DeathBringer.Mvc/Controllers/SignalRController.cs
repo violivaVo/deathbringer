30ae703722d4f3c4986507dbe507e894e107fe24
Mauro Bussini <mauro.bussini@hotmail.it>
2019-02-21 11:50:46 +0100

    Aggiunto seeding per SignalR

---
+++ DeathBringer.Mvc/Controllers/SignalRController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeathBringer.Mvc.Controllers
+{
+    public class SignalRController
+    {
+        //https://docs.microsoft.com/en-us/aspnet/core/tutorials/signalr?view=aspnetcore-2.2&tabs=visual-studio
+    }
+}
